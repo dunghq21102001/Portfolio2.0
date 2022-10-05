258b3f2d3b4bbf4178c2c5cd6bc2fa3bf90a508d
dunghq21102001 <dunghq21102001@gmail.com>
2022-10-05 19:12:24 +0700

    update source code in Experience.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -6,3 +6,7 @@ export const portfolioData = [
     { id: '5', src: 'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg' },
     { id: '6', src: 'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg' },
 ]
+
+export const experienceData = [
+    { id: 1, name: 'DFLEX WEBSITE', brief: 'Brief: React application with logging and register new user, save favorite movie to your list', tech: 'React, Tailwind CSS, Movie API, Firebase.', role: 'Front-end developer.', contribute: 'Personal project', github: 'https://github.com/dunghq21102001/DHQmovie.git' },
+]
